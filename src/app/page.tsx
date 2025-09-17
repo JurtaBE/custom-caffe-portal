d9c38160b60e517c91435f2b8d2ca0913c039f50
JurtaBE <jurta.contact@gmail.com>
2025-09-18 01:30:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-// remplace ceci (qui suppose un alias @ configuré)
-// import CustomCaffePortal from "@/components/CustomCaffePortal";
-
-// par ceci :
 import CustomCaffePortal from "../components/CustomCaffePortal";
 
 export default function Page() {
